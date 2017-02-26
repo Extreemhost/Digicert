570f14028fb6c0c295a4537481179d0ad284cc49
Fabrice Delahaij <admin@extreemhost.nl>
2017-02-26 18:34:33 +0100

    Version 1.0

---
+++ README.md
@@ -0,0 +1,27 @@
+## Digicert API Client
+
+
+## Requirements ##
+To use the Digicert API client, the following things are required:
+
++ Access to digicert API 
++ PHP >= 5.5
++ PHP cURL extension
+
+## Getting started ##
+
+```php
+
+	require_once('class.digicert.php');
+
+	$client = new DigiCert('[Account]:[API AUTH]');
+	$request = array(
+		'server_type'	=> 2,
+		'csr'		=> ''
+	);
+	$client->_call('/111222', $request);
+```
+
+## API documentation ##
+
+Please visit the [Digicert API Documentation](https://www.digicert.com/clients/rest/docs/retail).
---
+++ src/class.digicert.php
@@ -0,0 +1,32 @@
+<?php
+	/*
+	* DigiCert REST API 
+	*
+	* url: https://extreemhost.nl
+	*/
+	class DigiCert
+	{
+		protected $url = 'https://api.digicert.com/order';
+		protected $auth;
+
+		public function __construct($auth) {
+ 			$this->auth = $auth;
+    		}
+
+		function _call($location, $data) {
+			$return = json_encode($data);
+			$headers = array(
+				'Content-Type: application/vnd.digicert.rest-v1+json'
+			);
+			$request = curl_init();
+			curl_setopt($request, CURLOPT_URL, $this->url.$location);
+			curl_setopt($request, CURLOPT_USERPWD, $this->auth);
+			curl_setopt($request, CURLOPT_RETURNTRANSFER, 1);
+			curl_setopt($request, CURLOPT_CUSTOMREQUEST, 'REISSUE');
+			curl_setopt($request, CURLOPT_POSTFIELDS, $return);
+			curl_setopt($request, CURLOPT_HTTPHEADER, $headers);
+			$result = curl_exec($request);
+			curl_close($request);
+			return $result;
+		}
+	}
